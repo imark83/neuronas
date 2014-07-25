0f9a22588f5d9b5ad07827c936aa3d91c2eceb4b
Marcos Rodríguez Rodríguez <marcos@targaryen.local>
2014-07-25 20:49:10 +0200

    octave files deleted

--- openCL/taylorPartials/dibuja.m
+++
@@ -1,8 +0,0 @@
-clf
-xx=linspace(-5.00000e-01,7.00000e-01,64);
-yy=linspace(-5.00000e-01,5.00000e-01,64);
-F = load ("FLI.txt");
-[x,y] = meshgrid (xx,yy);
-pcolor(x,y,F)
-shading flat
-axis([-5.00000e-01 7.00000e-01 -5.00000e-01 5.00000e-01])
