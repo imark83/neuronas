9a67b91f577e394ea233e56612ff64bf6633a984
iMark83 <marcos@unizar.es>
2016-01-20 21:31:37 +0100

    code added for excite-excite and inhibit inhibit for peace makers

---
+++ control/excite_excite/peace_maker/fun.h
@@ -0,0 +1,54 @@
+void fS (real_t *rop, real_t *x) {
+
+		
+	real_t m_NaInf = 1.0 / (1.0 + exp (-150.0*(x[0] + 0.0305)));
+	real_t h_NaInf = 1.0 / (1.0 + exp (500.0*(x[0] + 0.0325)));
+	real_t m_K2Inf = 1.0 / (1.0 + exp (-83.0*(x[0] + 0.018 + VSHIFT)));
+
+	real_t I_Na = GNA * (x[0] - ENA) * x[1] * m_NaInf * m_NaInf * m_NaInf;
+	real_t I_K2 = GK2 * (x[0] - EK) * x[2] * x[2];
+	real_t I_L = GL * (x[0] - EL);
+
+	rop[0] = (-I_Na - I_K2 - I_L - IAPP) / C;
+	rop[1] = (h_NaInf - x[1]) / TNA;
+	rop[2] = (m_K2Inf - x[2]) / TK2;
+
+}
+
+void fN (real_t *rop, real_t *x, char pulse) {
+
+	unsigned char j;		// counter for neurons
+	real_t gamma[3];
+
+	// DEFINITION OF GAMMA(i)
+	for (j=0; j<3; j++) gamma[j] = 1.0 / (1.0 + exp (-1000.0*(x[3*j] - THSYN)));
+		
+	
+	for (j=0; j<3; j++) {
+		real_t I_syn;
+		if (j == 0) I_syn = GSYN * (x[0] - ESYN) * (gamma[1]+gamma[2]);
+		if (j == 1) I_syn = GSYN * (x[3] - ESYN) * (gamma[0]+gamma[2]);
+		if (j == 2) I_syn = GSYN * (x[6] - ESYN) * (gamma[0]+gamma[1]);
+
+		real_t m_NaInf = 1.0 / (1.0 + exp (-150.0*(x[3*j] + 0.0305)));
+		real_t h_NaInf = 1.0 / (1.0 + exp (500.0*(x[3*j] + 0.0325)));
+		real_t m_K2Inf = 1.0 / (1.0 + exp (-83.0*(x[3*j] + 0.018 + VSHIFT*(1.0 +0.0001*j*0))));
+
+		real_t I_Na = GNA * (x[3*j] - ENA) * x[1+3*j] * m_NaInf * m_NaInf * m_NaInf;
+		real_t I_K2 = GK2 * (x[3*j] - EK) * x[2+3*j] * x[2+3*j];
+		real_t I_L = GL * (x[3*j] - EL);
+
+		rop[3*j] = (-I_Na - I_K2 - I_L - IAPP - I_syn) / C;
+		rop[1+3*j] = (h_NaInf - x[1+3*j]) / TNA;
+		rop[2+3*j] = (m_K2Inf - x[2+3*j]) / TK2;
+
+	}
+	if (pulse==1) rop[0] -= 0.02 / C;
+	if (pulse==1) rop[3] -= 0.02 / C;
+	if (pulse==1) rop[6] -= 0.02 / C;
+
+	if (pulse==2) rop[0] += 0.01 / C;
+	if (pulse==2) rop[3] += 0.01 / C;
+	if (pulse==2) rop[6] += 0.01 / C;
+
+}
